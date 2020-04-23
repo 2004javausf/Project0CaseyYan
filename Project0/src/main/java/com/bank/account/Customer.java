79d835070bb70ca4dd56e86a240634c645b5e6c5
Casey Spencer <spencerc2009@my.fit.edu>
2020-04-23 14:35:07 -0400

    set up maven properly this time

---
+++ Project0/src/main/java/com/bank/account/Customer.java
@@ -0,0 +1,17 @@
+package com.bank.account;
+
+public class Customer {
+
+	private String name;
+	public Customer(String name, String address, String username, String password) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.username = username;
+		this.password = password;
+	}
+	private String address;
+	private String username;
+	private String password;
+	
+}
