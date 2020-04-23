900d7dbd2c74ca93ec1146068d962b614791df41
Casey Spencer <spencerc2009@my.fit.edu>
2020-04-23 14:38:48 -0400

    readability

--- Project0/src/main/java/com/bank/account/Customer.java
+++ Project0/src/main/java/com/bank/account/Customer.java
@@ -3,6 +3,10 @@ package com.bank.account;
 public class Customer {
 
 	private String name;
+	private String address;
+	private String username;
+	private String password;
+	
 	public Customer(String name, String address, String username, String password) {
 		super();
 		this.name = name;
@@ -10,8 +14,4 @@ public class Customer {
 		this.username = username;
 		this.password = password;
 	}
-	private String address;
-	private String username;
-	private String password;
-	
 }
